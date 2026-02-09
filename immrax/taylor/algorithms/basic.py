feae298341972736e2c2decc55533fd03b5223e0
Akash Harapanahalli <akash.harapanahalli@gmail.com>
2026-02-09 11:44:16 -0500

    working on basic alg

--- immrax/taylor/algorithms/basic.py
+++ immrax/taylor/algorithms/basic.py
@@ -4,13 +4,13 @@ import jax.numpy as jnp
 from immrax.inclusion import Interval, interval, natif
 from immrax.system import System
 from immrax.utils import inv_fact, prolongation
-from .. import TaylorModel, TaylorPolynomial, nattm, nattp
+from .. import taylor_model, TaylorModel, TaylorPolynomial, nattm, nattp
 from .base import TMFlowpipeGenerator
 
 from typing import Tuple
 
 
-def tx_tm_eval(tm: TaylorModel, t: float, t_order: int) -> TaylorModel:
+def tx_tm_eval(tm: TaylorModel, t: float) -> TaylorModel:
     """Evaluate a time-dependent Taylor Model at a given time.
 
     Parameters
@@ -32,19 +32,20 @@ def tx_tm_eval(tm: TaylorModel, t: float, t_order: int) -> TaylorModel:
     # in [0,0,0,...,1,1,1,...,t_order,t_order,t_order]
     # i.e., tm.exponents[1:, L * i : L * (i + 1)] is the same for every i
 
-    L = (t_order + 1) // tm.exponents.shape[0]
+    t_order = tm.exponents[0, -1]
+    L = tm.exponents.shape[1] // (t_order + 1)
 
     exponents = tm.exponents[1:, 0:L]
-    coeffs_list = [tm.coeffs[1:, L * i : L * (i + 1)] for i in range(t_order + 1)]
+    coeffs_list = jnp.split(tm.coeffs, t_order + 1, axis=1)
     coeffs = jnp.sum(
-        jnp.asarray(coeffs_list) * (t ** jnp.arange(t_order + 1)),
+        jnp.asarray(coeffs_list) * (t ** jnp.arange(t_order + 1))[:, None, None],
         axis=0,
     )
 
     return TaylorModel(
         coeffs=coeffs,
         exponents=exponents,
-        remainder=tm.remainder[1:],
+        remainder=tm.remainder,
         flat_domain=tm.flat_domain[1:],
         flat_center=tm.flat_center[1:],
         _domain_treedef=tm._domain_treedef.children()[1],
@@ -52,13 +53,44 @@ def tx_tm_eval(tm: TaylorModel, t: float, t_order: int) -> TaylorModel:
         _per_leaf_order=tm._per_leaf_order[1:],
     )
 
-def 
+
+def tps_to_tx(
+    polys: list[TaylorPolynomial], remainder, domain, center
+) -> TaylorPolynomial:
+    """Convert a list of TaylorPolynomials to a single TaylorPolynomial with canonical
+    coefficient structure, over the domain
+
+    Parameters
+    ----------
+    polys : list[TaylorPolynomial]
+        List of TaylorPolynomials to convert.
+
+    Returns
+    -------
+    TaylorPolynomial
+        The TaylorPolynomial with canonical coefficient structure.
+    """
+    t_order = len(polys) - 1
+    mon_len = polys[0].exponents.shape[-1]
+    coeffs = jnp.concatenate(
+        [polys[i].coeffs * inv_fact(i) for i in range(t_order + 1)], axis=1
+    )
+    exponents_top = jnp.repeat(jnp.arange(t_order + 1), mon_len)
+    exponents_bottom = jnp.concatenate([p.exponents for p in polys], axis=1)
+
+    return taylor_model(
+        coeffs=coeffs,
+        exponents=jnp.vstack((exponents_top, exponents_bottom)),
+        remainder=remainder,
+        domain=domain,
+        center=center,
+    )
 
 
 class BasicTMFlowpipeGenerator(TMFlowpipeGenerator):
     """A basic implementation of a Picard-based flowpipe generator."""
 
-    def _initialize(self, t0, tf, tm0, dt_max, *, t_order, max_steps, **kwargs):
+    def _initialize(self, t0, tf, tm0, dt_max, *, t_order, max_steps=4096, **kwargs):
         """Initialize the algorithm before calling generate_flowpipe."""
         self.t_order = t_order
         self._prolonged_f = prolongation(self.sys.f, t_order)
@@ -69,9 +101,12 @@ class BasicTMFlowpipeGenerator(TMFlowpipeGenerator):
 
     def _step(self, t: float, tmi: TaylorModel, dt_max: float, **kwargs):
         # Step 1: Compute the Taylor expansion of the flow map to t_order, x_order
-        # TODO: Fix the nattp module and change center propagation
 
-        # poly = nattm(self._prolonged_f, structured_center=True)(tmi).polynomial
-
-        tt = jnp.arange(self.t_order + 1) * inv_fact(jnp.arange(self.t_order + 1))
-        poly = nattp(get_poly)(tmi.polynomial)
+        poly_coeffs = nattp(lambda x: self._prolonged_f(t, x))(tmi.polynomial)
+        poly = tps_to_tx(
+            poly_coeffs,
+            tmi.remainder,
+            (interval(jnp.asarray(t), jnp.asarray(t + dt_max)), tmi.domain),
+            (t, tmi.center),
+        )
+        return poly
